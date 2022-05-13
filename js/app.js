2fcd431ba7b6cd37ab200956bc77aa84ecc51a2a
Oleksii Pyzhyk <watt-kolomiya@rambler.ru>
2022-05-13 16:53:48 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,20 +1,14 @@
-var count = ['1', '2', '3']
-var art = ['Julius Caesar', 'Into the Abyss', 'Discobolous']
-var price = ['221', '157', '95']
-var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    effect: 'fade',
-    fadeEffect: {
-        crossFade: true
-    },
-    slidesPerView: 1,
-    spaceBetween: 30,
-    mousewheel: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<p class="' + className + '">' + ('#0' + (count[index])) + '<span class="pagination-art">' + (art[index]) + '</span>' + '<span class="pagination-price">' + '<span>' + ((price[index]) + '</span>' + ' ' + 'ETH') + '</span>' + "</p>";
-      },
-    },
-  });
+let header = document.querySelector('.js-header'),
+    headerH = document.querySelector('.js-header').clientHeight;
+
+document.onscroll = function () {
+    let scroll = window.scrollY;
+
+    if (scroll > headerH + 150) {
+        header.classList.add('fixed');
+        document.body.style.paddingTop = headerH + 'px';
+    } else {
+        header.classList.remove('fixed');
+        document.body.removeAttribute('style');
+    }
+}
